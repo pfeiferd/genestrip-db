c15afe394c6de0c849f5d3c2a07737cd6e837889
Daniel Pfeifer <daniel.pfeifer@hs-heilbronn.de>
2024-09-23 10:28:14 +0200

    Updated for rna stuff.

--- bin/makedbs.sh
+++ bin/makedbs.sh
@@ -15,12 +15,13 @@ fi
 
 mvn exec:exec@db -Dname=human_virus2 -Dgoal=$goal
 mvn exec:exec@db -Dname=babesia -Dgoal=$goal
-mvn exec:exec@db -Dname=chronicb-rna -Dgoal=$goal
 mvn exec:exec@db -Dname=protozoa -Dgoal=$goal
+mvn exec:exec@db -Dname=protozoa-rna -Dgoal=$goal
 mvn exec:exec@db -Dname=vineyard -Dgoal=$goal
 mvn exec:exec@db -Dname=parasites -Dgoal=$goal
 mvn exec:exec@db -Dname=borrelia -Dgoal=$goal
 mvn exec:exec@db -Dname=borrelia_plasmid -Dgoal=$goal
 mvn exec:exec@db -Dname=chronicb -Dgoal=$goal
+mvn exec:exec@db -Dname=chronicb-rna -Dgoal=$goal
 mvn exec:exec@db -Dname=plasmopara -Dgoal=$goal
 
